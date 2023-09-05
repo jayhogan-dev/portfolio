a5efc609109241a1164425e629884f9a8534411f
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-05 12:43:10 -0700

    added: logo and default styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary-blue": {
+          light: "#155e75",
+          dark: "#06b6d4",
+        },
+        secondary: "#fde047",
+        "primary-white": {
+          500: "#6F74A7",
+          800: "#F3F8FF",
+        },
+        "primary-black": {
+          200: "#151E2C",
+          300: "#29374C",
+          400: "#778295",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
