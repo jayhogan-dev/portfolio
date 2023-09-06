2fd5c13bf72527d6cde23d5148cba6c00005e20f
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-06 11:33:41 -0700

    feat: image added and mobile responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,36 +3,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-start pt-12 gap-4">
-      <h2 className="font-semibold text-primary-blue-light dark:text-primary-blue-dark text-sm uppercase leading-5 tracking-[4px]">
-        Hi, I am Jay
-      </h2>
-      <h1 className="text-primary-black-200 dark:text-white text-4xl leading-[130%] font-bold w-[320px]">
-        Professional Web Developer based in USA
-        <div className="relative -top-16 bg-gradient-to-r from-cyan-800 via-cyan-400 to-yellow-300 h-4 w-[280px] -z-10" />
-      </h1>
-      <p className="text-xs leading-[155%] text-primary-white-500 dark:text-primary-white-800">
-        Elevating the digital landscape through meticulous craftsmanship, I
-        program immersive web experiences that blend innovation and artistry
-        seamlessly.
-      </p>
-      <div className="flex flex-col w-full gap-4">
-        <Button>My Work</Button>
-        <Button variant="default">
-          <div className="flex w-full justify-between items-center">
-            <span className="text-primary-white-500">
-              jayhogan.dev@gmail.com
-            </span>
-            <Image
-              src="/web-icons/frame.svg"
-              alt="copy"
-              width={20}
-              height={20}
-            />
-          </div>
-        </Button>
+    <main className="flex flex-col items-center pt-6 gap-6 md:flex-row md:justify-between lg:gap-10 xl:gap-32">
+      <div className="flex flex-col gap-4 w-full lg:w-2/5">
+        <h2 className="font-semibold text-primary-blue-light dark:text-primary-blue-dark text-sm lg:text-xl uppercase leading-5 tracking-[4px] lg:tracking-[6px]">
+          Hi, I am Jay
+        </h2>
+        <h1 className="text-primary-black-200 dark:text-white text-4xl leading-[115%] lg:text-heading2 xl:text-heading1 font-bold w-[320px] lg:w-[400px] xl:w-[500px]">
+          Professional Web Developer based in USA
+          <div className="relative -top-14 bg-gradient-to-r from-cyan-800 via-cyan-400 to-yellow-300 h-4 w-[280px] lg:w-[370px] lg:-top-[70px] xl:h-6 xl:w-[490px] xl:-top-[110px] -z-10" />
+        </h1>
+        <p className="text-small-regular lg:text-body-regular text-primary-white-500 dark:text-primary-white-800">
+          Elevating the digital landscape through meticulous craftsmanship, I
+          program immersive web experiences that blend innovation and artistry
+          seamlessly.
+        </p>
+        <div className="flex flex-col w-full gap-4 lg:flex-row">
+          <Button className="lg:w-1/3">My Work</Button>
+          <Button
+            variant="default"
+            className="lg:w-2/3"
+          >
+            <div className="flex w-full justify-between items-center truncate">
+              <span className="text-primary-white-500">
+                jayhogan.dev@gmail.com
+              </span>
+              <Image
+                src="/web-icons/frame.svg"
+                alt="copy"
+                width={20}
+                height={20}
+              />
+            </div>
+          </Button>
+        </div>
       </div>
-      <div className="relative flex w-full h-64">
+      <div className="relative flex w-full h-72 xs:h-96 sm:w-[80%] md:w-full lg:w-2/3 items-center lg:h-[550px] ">
         <Image
           src="/hero.png"
           alt="copy"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      screens: {
+        xs: "500px",
+      },
       fontSize: {
         heading1: [
           "64px",
