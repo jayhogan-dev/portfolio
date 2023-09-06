28ba885f1534150f6a2d73187681ff09d26d1616
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-06 12:10:57 -0700

    feat: button size updated

--- app/page.tsx
+++ app/page.tsx
@@ -13,15 +13,21 @@ export default function Home() {
           <div className="relative -top-14 bg-gradient-to-r from-cyan-800 via-cyan-400 to-yellow-300 h-4 w-[280px] lg:w-[370px] lg:-top-[70px] xl:h-6 xl:w-[490px] xl:-top-[110px] -z-10" />
         </h1>
         <p className="text-small-regular lg:text-body-regular text-primary-white-500 dark:text-primary-white-800">
-          Elevating the digital landscape through meticulous craftsmanship, I
-          program immersive web experiences that blend innovation and artistry
-          seamlessly.
+          Transforming the web one line of code at a time: Crafting cutting-edge
+          digital experiences with precision, passion, and a profound commitment
+          to excellence
         </p>
         <div className="flex flex-col w-full gap-4 lg:flex-row">
-          <Button className="lg:w-1/3">My Work</Button>
+          <Button
+            className="lg:w-1/3"
+            size="lg"
+          >
+            My Work
+          </Button>
           <Button
             variant="default"
             className="lg:w-2/3"
+            size="lg"
           >
             <div className="flex w-full justify-between items-center truncate">
               <span className="text-primary-white-500">
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-white hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -22,8 +22,8 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        sm: "h-9 rounded-3xl px-3",
+        lg: "h-11 rounded-3xl px-8",
         icon: "h-10 w-10",
       },
     },
