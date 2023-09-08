beb385db54bcc6d3dcd82beac92592614a538edc
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-08 09:03:38 -0700

    footer added

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -21,10 +21,10 @@ const Skills = () => {
           {skills.map((skill) => (
             <div
               key={skill.label}
-              className="relative w-16 h-16 md:h-20 md:w-20 lg:h-24 lg:w-24"
+              className="relative flex items-center justify-center"
             >
               <Circle className="fill-primary-white-800 dark:fill-primary-black-300" />
-              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-8 h-8 md:h-16 md:w-12 lg:h-12 lg:w-16">
+              <div className="absolute w-12 h-12">
                 <TooltipProvider>
                   <Tooltip>
                     <TooltipTrigger asChild>
