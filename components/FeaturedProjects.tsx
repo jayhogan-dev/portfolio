6a28e5061d3ef06de83d1b61e8d3472c60e21ab8
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-11 08:52:07 -0700

    feat: reusable component added

--- components/FeaturedProjects.tsx
+++ components/FeaturedProjects.tsx
@@ -11,7 +11,30 @@ const FeaturedProjects = () => {
         </div>
         <div className="relative -top-14 -right-[85px] bg-gradient-to-r from-cyan-400 to-yellow-300 dark:from-cyan-800 dark:via-cyan-400 dark:to-yellow-300 h-4 w-[140px] lg:w-[190px] lg:-top-[56px] lg:left-[115px]" />
         {/* Projects */}
-        <FeaturedProjectsCard />
+        <div className="flex flex-col w-full gap-5">
+          <FeaturedProjectsCard
+            projectTitle="Morent"
+            projectDescription="A Car Rental"
+            frontend="React, Next.js"
+            backend="Next.js, MongoDB"
+            link="/"
+            bgColor="bg-blue-600"
+            technologiesUsedBgColor="bg-blue-400"
+            laptopImage="/morent-laptop.png"
+            mobileImage="/morent-mobile.png"
+          />
+          <FeaturedProjectsCard
+            projectTitle="JobIT"
+            projectDescription="A Job Finding"
+            frontend="React, Next.js"
+            backend="RapidAPI"
+            link="/"
+            bgColor="bg-emerald-500"
+            technologiesUsedBgColor="bg-emerald-400"
+            laptopImage="/jobit-laptop.png"
+            mobileImage="/jobit-mobile.png"
+          />
+        </div>
       </section>
     </LightContainer>
   );
--- components/FeaturedProjectsCard.tsx
+++ components/FeaturedProjectsCard.tsx
@@ -1,27 +1,51 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const FeaturedProjectsCard = () => {
+interface FeaturedProjectsCardProps {
+  bgColor: string;
+  technologiesUsedBgColor: string;
+  projectTitle: string;
+  projectDescription: string;
+  frontend: string;
+  backend: string;
+  link: string;
+  laptopImage: string;
+  mobileImage: string;
+}
+
+const FeaturedProjectsCard = ({
+  bgColor,
+  technologiesUsedBgColor,
+  projectTitle,
+  projectDescription,
+  frontend,
+  backend,
+  link,
+  laptopImage,
+  mobileImage,
+}: FeaturedProjectsCardProps) => {
   return (
-    <article className="flex flex-col w-full">
-      <div className="flex flex-col bg-blue-500 rounded-2xl py-11 px-6">
-        <div className="flex flex-col gap-7">
-          <h1 className="text-heading3 text-white whitespace-nowrap">
-            Morent -
-            <br className="md:hidden" /> A Car Rental{" "}
+    <article className="flex flex-col">
+      <div
+        className={`flex flex-col md:flex-row md:justify-between ${bgColor} rounded-2xl py-11 px-6 gap-7`}
+      >
+        <div className="flex flex-col gap-7 bg-red-300 md:w-[330px]">
+          <h1 className="text-heading3 text-white">
+            {projectTitle} -
+            <br className="md:hidden" /> {projectDescription}{" "}
             <br className="md:hidden" />
             Application
           </h1>
           <div className="flex w-full gap-3 text-center">
-            <div className="p-3 bg-blue-400 rounded-sm">
-              <p className="text-small-regular text-white">React, Next.js</p>
+            <div className={`p-3 ${technologiesUsedBgColor} rounded-sm`}>
+              <p className="text-small-regular text-white">{frontend}</p>
             </div>
-            <div className="p-3 bg-blue-400 rounded-sm">
-              <p className="text-small-regular text-white">Next.js, MongoDB</p>
+            <div className={`p-3 ${technologiesUsedBgColor} rounded-sm`}>
+              <p className="text-small-regular text-white">{backend}</p>
             </div>
           </div>
           <Link
-            href="/"
+            href={link}
             className="text-white text-body-bold items-center flex"
           >
             See Project Details
@@ -34,12 +58,22 @@ const FeaturedProjectsCard = () => {
             />
           </Link>
         </div>
-        <Image
-          src="/morent-laptop.png"
-          alt="laptop"
-          height={100}
-          width={300}
-        />
+        <div className="flex items-end md:pt-5">
+          <div className="relative w-[230px] h-32 md:w-[400px] md:h-[200px] z-10">
+            <Image
+              src={laptopImage}
+              alt="laptop"
+              fill
+            />
+          </div>
+          <div className="-ml-3 relative w-[60px] h-[112px] md:w-[90px] md:h-[160px]">
+            <Image
+              src={mobileImage}
+              alt="phone"
+              fill
+            />
+          </div>
+        </div>
       </div>
     </article>
   );
