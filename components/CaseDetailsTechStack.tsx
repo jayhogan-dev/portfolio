0f386edb27905b80c5e0fcdd0ea9278ca5c46007
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-17 13:34:48 -0700

    tech stack added

--- components/CaseDetailsTechStack.tsx
+++ components/CaseDetailsTechStack.tsx
@@ -1,10 +1,12 @@
+import { morentStack } from "@/constants";
+import TechStack from "./TechStack";
 import LightContainer from "./containers/Light";
 
 const CaseDetailsTechStack = () => {
   return (
     <LightContainer>
       <section className="flex flex-col py-12 gap-6 md:py-[72px] items-center justify-center w-full">
-        <div className="flex flex-col w-full sm:flex-row justify-between gap-5 md:gap-0 sm:w-[500px] md:w-[600px] lg:w-[880px]">
+        <div className="flex mb-5 flex-col w-full sm:flex-row justify-between gap-5 md:gap-0 sm:w-[500px] md:w-[600px] lg:w-[880px]">
           <div className="flex flex-col gap-3">
             <h3 className="text-small-bold md:text-body-bold text-primary-black-400 dark:text-primary-white-500">
               My Role
@@ -29,6 +31,13 @@ const CaseDetailsTechStack = () => {
               09/01/2023
             </h2>
           </div>
+        </div>
+        <div className="flex mt-5 flex-col gap-3 w-full sm:w-[500px] md:w-[600px] lg:w-[880px]">
+          <h3 className="text-xs text-primary-blue-light dark:text-primary-blue-dark font-semibold">
+            Technologies Used
+          </h3>
+          <h2 className="text-heading3 mb-2">Tech Stack</h2>
+          <TechStack data={morentStack} />
         </div>
       </section>
     </LightContainer>
