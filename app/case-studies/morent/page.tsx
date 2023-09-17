d9adaa50b1dfd1421b0f254c8be0758f3fb1ce3a
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-17 12:29:55 -0700

    headers added

--- app/case-studies/morent/page.tsx
+++ app/case-studies/morent/page.tsx
@@ -1,9 +1,11 @@
 import CaseDetailsHeader from "@/components/CaseDetailsHeader";
+import CaseDetailsTechStack from "@/components/CaseDetailsTechStack";
 
 const MorentPage = () => {
   return (
     <div className="flex flex-col">
       <CaseDetailsHeader />
+      <CaseDetailsTechStack />
     </div>
   );
 };
---
+++ components/CaseDetailsTechStack.tsx
@@ -0,0 +1,38 @@
+import LightContainer from "./containers/Light";
+
+const CaseDetailsTechStack = () => {
+  return (
+    <LightContainer>
+      <section className="flex flex-col py-12 gap-6 md:py-[72px] items-start justify-start w-full sm:w-[500px] md:w-[880px]">
+        <div className="flex flex-col bg-red-300 w-full sm:flex-row justify-between gap-5 md:gap-0">
+          <div className="flex flex-col gap-3">
+            <h3 className="text-small-bold md:text-body-bold text-primary-black-400 dark:text-primary-white-500">
+              My Role
+            </h3>
+            <h2 className="text-paragraph-bold md:text-base-bold text-primary-black-200 dark:text-white">
+              Full-Stack Developer
+            </h2>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h3 className="text-small-bold md:text-body-bold text-primary-black-400 dark:text-primary-white-500">
+              Start Date
+            </h3>
+            <h2 className="text-paragraph-bold md:text-base-bold text-primary-black-200 dark:text-white">
+              08/01/2023
+            </h2>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h3 className="text-small-bold md:text-body-bold text-primary-black-400 dark:text-primary-white-500">
+              End Date
+            </h3>
+            <h2 className="text-paragraph-bold md:text-base-bold text-primary-black-200 dark:text-white">
+              09/01/2023
+            </h2>
+          </div>
+        </div>
+      </section>
+    </LightContainer>
+  );
+};
+
+export default CaseDetailsTechStack;
