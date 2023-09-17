8ce0c84feef7786e50b540d59404846cc97502cd
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-17 12:36:59 -0700

    mobile responsive

--- components/CaseDetailsTechStack.tsx
+++ components/CaseDetailsTechStack.tsx
@@ -3,8 +3,8 @@ import LightContainer from "./containers/Light";
 const CaseDetailsTechStack = () => {
   return (
     <LightContainer>
-      <section className="flex flex-col py-12 gap-6 md:py-[72px] items-start justify-start w-full sm:w-[500px] md:w-[880px]">
-        <div className="flex flex-col bg-red-300 w-full sm:flex-row justify-between gap-5 md:gap-0">
+      <section className="flex flex-col py-12 gap-6 md:py-[72px] items-center justify-center w-full">
+        <div className="flex flex-col w-full sm:flex-row justify-between gap-5 md:gap-0 sm:w-[500px] md:w-[600px] lg:w-[880px]">
           <div className="flex flex-col gap-3">
             <h3 className="text-small-bold md:text-body-bold text-primary-black-400 dark:text-primary-white-500">
               My Role
