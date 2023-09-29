9e8d0bbd6995f8adfa1cb9f338039c0b6236d5d7
Jay Hogan <jayhogan.dev@gmail.com>
2023-09-28 17:07:05 -0700

    pulling in sanity data

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -24,8 +24,7 @@ const BlogPage = async () => {
           <h1 className="relative text-4xl text-center font-bold leading-[115%] tracking-[-0.36px] lg:text-heading1 dark:text-white text-primary-black-300 mb-10 z-10">
             Blogs
           </h1>
-          <div className="xxs:hidden relative -top-24 -right-[66px] bg-gradient-to-r from-cyan-400 to-yellow-300 dark:from-cyan-800 dark:via-cyan-400 dark:to-yellow-300 h-4 w-[88px]" />
-          <div className="relative -top-[70px] bg-gradient-to-r from-cyan-400 to-yellow-300 dark:from-cyan-800 dark:via-cyan-400 dark:to-yellow-300 h-4 w-[130px] xxs:-top-14 xxs:w-[230px] xxs:-right-[68px] lg:w-[415px] lg:-top-[64px] lg:-right-[120px]" />
+          <div className="relative -top-[56px] bg-gradient-to-r from-cyan-400 to-yellow-300 dark:from-cyan-800 dark:via-cyan-400 dark:to-yellow-300 h-4 w-[100px] lg:w-[173px] lg:-top-[64px] lg:-left-[0px]" />
           <p className="-mt-5 text-small-regular lg:text-paragraph-regular text-primary-white-500 dark:text-primary-white-800 w-full sm:w-[500px] lg:w-[650px] text-center">
             Explore Insights, Tutorials, and Stories on Programming and Web
             Development
